9e547ce8f995a987871445d9799443ed246994e1
lerakorzhova <lerakorzhova98@gmail.com>
2017-02-19 18:30:57 +0300

    brackets: withiot 10, 17 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var arr = [],
+        len = str.length,
+        count = 0;
+
+    
+
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (str.charAt(i) === bracketsConfig[j][0]) {
+                arr.push(str.charAt(i));
+            }
+            if (str.charAt(i) === bracketsConfig[j][1]) {
+                if (arr[arr.length - 1] === bracketsConfig[j][0]) {
+                    arr.pop();
+                }
+            }
+            console.log(arr)
+        }
+    }
+    if (arr.length === 0) return true;
+    return false;
 }
